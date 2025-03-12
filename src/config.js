7a52beb4ae0c9de7da4b07284aa175bea5a5fdb1
Harikanth2307 <harikanth2307@gmail.com>
2025-03-12 17:54:42 +0000

    changes in public/index

--- src/config.js
+++
@@ -1,6 +0,0 @@
-// 🚨 Vulnerability: Hardcoded API Key
-export const API_KEY = "12345-FAKE-API-KEY"; 
-
-fetch(`https://api.example.com/data?api_key=${API_KEY}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
